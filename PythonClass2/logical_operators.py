42f1d3bf937e1d6033c455b709bf88d618b961e5
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-02-07 14:39:13 -0300

    booleans

---
+++ PythonClass2/logical_operators.py
@@ -0,0 +1,12 @@
+# AND (and): Retorna True se ambas as expressões são verdadeiras.
+# OR (or): Retorna True se pelo menos uma das expressões é verdadeira.
+# NOT (not): Inverte o valor da expressão booleana.
+
+p = True
+q = False
+
+resultado_and = p and q
+resultado_or = p or q
+resultado_not = not p
+
+print(resultado_and, resultado_or, resultado_not)
