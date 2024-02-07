7a68388c2ea7e0e40b71b0eb763130937044a0fe
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-02-07 13:51:21 -0300

    variables

---
+++ PythonClass2/operation_variables.py
@@ -0,0 +1,17 @@
+# Operações com Variáveis
+salario = 3000
+bonus = 500
+salario_total = salario + bonus
+print(f'O salário total é de: R${salario_total},00')
+
+
+# Manipulação de Strings
+nome = "Maria"
+sobrenome = "Silva"
+nome_completo = nome + " " + sobrenome
+print(f'O nome completo é {nome_completo}')
+
+# Atualização de Variáveis
+contador = 0
+contador = contador + 1
+print(contador)
