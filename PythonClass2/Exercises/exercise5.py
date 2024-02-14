3c24aa9c9541cbd0340f9dd0a0522613dd7efc11
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-02-14 13:36:45 -0300

    exercise_convertion

--- PythonClass2/Exercises/exercise5.py
+++ PythonClass2/Exercises/exercise5.py
@@ -4,7 +4,6 @@
 # Calcule as raízes da equação usando a fórmula de Bhaskara.
 import math
 
-
 a = 1
 b = -10
 c = 25
@@ -25,6 +24,21 @@ print(x2)
 # Implemente um programa que converta um valor em dólares para outras moedas 
 # (por exemplo, euros e libras). Solicite ao usuário o valor em dólares e a taxa de conversão.
 
-value = int(input("Informe o valor: "))
-taxa = int(input("Informe a taxa de conversão: "))
+option = int(input("Informe a conversão desejada! 1 - Euro | 2 - Reais"))
+
+
+taxa_euro = 0.93
+taxa_real = 4.97
+
+if option == 1:
+    value = float(input("Informe o valor em dólares: "))
+    euro = taxa_euro * value
+    print(f"O valor correspondente é {euro} euros!")
+elif option != 1 or option != 2:
+    print("ERRO!")
+else:
+    value = float(input("Informe o valor em dólares: "))
+    real = taxa_real * value
+    print(f"O valor correspondente é {real} reais!")
+    
 
