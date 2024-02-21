beffdfb7a5c0e863764c4fdf3f39a32f07f32d7b
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-02-21 14:46:21 -0300

    ex_4

---
+++ PythonClass4/Exercises/ex4.py
@@ -0,0 +1,8 @@
+# Leia um vetor de 10 caractreres e digas quantas consoantes foram lidas. Imprima as consoantes
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+consonants = ["a", "e", "i", "o", "u"]
+cont = 0
+
+for letters in consonants:
+    if consonants == letters:
+        print(letters)
