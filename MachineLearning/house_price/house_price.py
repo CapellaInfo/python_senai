7fa55d0e5d2bfb0905400c54343bd67665bfa66f
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-02-26 16:53:28 -0300

    machine_learning

--- MachineLearning/house_price/house_price.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-# from pandas_profiling import ProfileReport
-pd.options.display.float_format = '{:,.2f}'.format
-# %matplotlib inline
-
-base = pd.read_csv("melb_data.csv")
