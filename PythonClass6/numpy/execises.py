067ffa804bc279cf09eb1d5859982f8feade1492
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-03-06 16:56:59 -0300

    exercises

---
+++ PythonClass6/numpy/execises.py
@@ -0,0 +1,15 @@
+import numpy as np
+# Como criar uma matriz NumPy vazia e uma matriz preenchida com valores iguais a 1?
+
+# Crie uma matriz NumPy preenchida com todos os zeros.
+
+
+# Crie uma matriz NumPy preenchida com valores aleatórios entre 0 e 1.
+
+
+# Crie um array unidimensional com os números de 1 a 5.
+
+
+# Crie um array bidimensional (matriz) 2x3 com números inteiros.
+
+
