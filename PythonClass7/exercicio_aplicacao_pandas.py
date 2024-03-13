075af8886abdebfaccad47417b256d3d1d486635
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-03-13 14:54:33 -0300

    pandas

--- PythonClass7/exercicio_aplicacao_pandas.py
+++ PythonClass7/exercicio_aplicacao_pandas.py
@@ -17,3 +17,21 @@ print("============================")
 print(df.info())  # Informações sobre os tipos de dados e valores nulos
 print("============================")
 print(df.describe()) # Estatísticas descritivas básicas
+
+# Seleção e Filtragem de Dados
+print("Funcionários com idade maior que 30 anos")
+funcionario_maior_30 = df[df['idade'] > 30]
+print(funcionario_maior_30)
+
+print("Funcionários com Salário maior que 4000")
+funcionario_maior_4000 = df[df['salario'] > 4000]
+print(funcionario_maior_4000)
+
+print("Funcionário(a) com maior salário")
+maior_salario = df.max()
+print(maior_salario)
+
+# Manipulação de dados
+print("Salário com aumento de 10%: ")
+df['salario_aumento'] = df['salario'] * 1.10
+print(df)
