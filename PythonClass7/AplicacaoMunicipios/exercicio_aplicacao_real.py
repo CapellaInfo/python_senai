b01186057c6395d597d6d1b2c626093d9d4ffeca
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-03-13 16:10:23 -0300

    exercicio_aplicacao_real

---
+++ PythonClass7/AplicacaoMunicipios/exercicio_aplicacao_real.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+igm_df = pd.read_csv("PythonClass7/APlicacaoMunicipios/igm_modificado.csv")
+
+print(igm_df.head())
+print(igm_df.shape)
+print(igm_df.info())
+print(igm_df.tail())
+print(igm_df.sample(5))
+print(igm_df.sample(5).T)
+print(igm_df[0:5].T)
+print(igm_df[-5:].T)
+print(igm_df[20:30])
+print(igm_df['porte'])
+print(igm_df['porte'].value_counts())
+ind_des = igm_df['indice_governanca']
+ind_des.count()
+ind_des.size
+ind_des.isnull()
+ind_des.isnull().sum()
+ind_des.dropna()
+ind_des.isnull().sum()
+ind_des.dropna(inplace=True)
+ind_des.isnull().sum()
+ind_des.min()
+ind_des.max()
+ind_des.mean()
+ind_des.std()
+ind_des.describe()
+igm_df.describe()
+igm_df[igm_df['regiao']=='NORDESTE']
