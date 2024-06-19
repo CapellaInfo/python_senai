0260ea63cd61c9d150f48c72f07e668f69678b5a
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-06-19 16:05:27 -0300

    streamlit

--- PythonClass19/analise_vendas/app.py
+++ PythonClass19/analise_vendas/app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 
 @st.cache_data
--- PythonClass20/exemplo_widgets/app.py
+++ PythonClass20/exemplo_widgets/app.py
@@ -1,17 +1,29 @@
 import streamlit as st
 import pandas as pd
 
-df1 = pd.read_csv("vendas.csv")
+df = pd.read_csv("vendas.csv")
 
 # Seleção de intervalo de datas
 st.sidebar.header('Filtro por Data')
 data_inicio = pd.Timestamp(st.sidebar.date_input('Data Início', value=pd.to_datetime('2023-01-01')))
 data_fim = pd.Timestamp(st.sidebar.date_input('Data Fim', value=pd.to_datetime('2023-12-31')))
 
-df2 = df1[(pd.to_datetime(df1['data']) >= data_inicio) & (pd.to_datetime(df1['data']) <= data_fim)]
+df2 = df[(pd.to_datetime(df['data']) >= data_inicio) & (pd.to_datetime(df['data']) <= data_fim)]
 
 st.title('Dashboard de Vendas Interativo')
 st.write(df2)
+
+# Slider para selecionar intervalo de valores de vendas
+st.sidebar.header('Filtro por Vendas')
+vendas_min, vendas_max = st.sidebar.slider('Vendas', float(0), float(1000), (float(df['vendas'].min()), float(df['vendas'].max())))
+
+
+# Filtrar dados pelo intervalo de valores de vendas
+df_filtrado2 = df[(df['vendas'] >= vendas_min) & (df['vendas'] <= vendas_max)]
+
+
+st.write(df_filtrado2)
+
 
 
 
